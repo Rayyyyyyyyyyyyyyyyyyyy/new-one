4dee2785e291bd194ed41ecb41fc8f079d285bb4
Ray <ray@fillygaming.com>
2022-01-10 10:59:43 +0800

    deploy

---
+++ precache-manifest.aae3e91e369de1cd73c36e60a0a1a8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55607722776a659ca8f",
+    "url": "/new-one/css/app.ca936927.css"
+  },
+  {
+    "revision": "58972c68a3b12110ab8d34cce615200d",
+    "url": "/new-one/index.html"
+  },
+  {
+    "revision": "798b9776acb75f98b6a8",
+    "url": "/new-one/js/about.b8768a94.js"
+  },
+  {
+    "revision": "c55607722776a659ca8f",
+    "url": "/new-one/js/app.5a00aa0a.js"
+  },
+  {
+    "revision": "fc457dc5151b06e4aaed",
+    "url": "/new-one/js/chunk-vendors.13cb6ccc.js"
+  },
+  {
+    "revision": "3ea266d78b15881f43bdbc88439f098b",
+    "url": "/new-one/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/new-one/robots.txt"
+  }
+]);
